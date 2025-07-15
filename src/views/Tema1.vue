0784024dfc2d380a413275eabd2173235c8a3b3d
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2025-07-14 21:33:37 -0500

    Initial commit

---
+++ src/views/Tema1.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido(data-aos="fade-right")
+      .titulo-principal__numero
+        span 1
+      h1 Titulo_de_primer_nivel
+
+    //- El componente "Muestras" tiene las aplicaciones de todos los componentes de la base
+    //- se encuentra en src/components/Muestras.vue
+    //- Una vez el componente "Muestras" no se necesite 
+    //- Se debe borrar el "import Muestras from '../components/Muestras'" y en "components" en "<script"
+    //- esto evitará que se compile en la carpeta final de distribución
+    Muestras
+
+</template>
+
+<script>
+// eslint-disable-next-line prettier/prettier
+import Muestras from '../components/Muestras'; // borrar una vez el componente "Muestras" no se necesite
+export default {
+  name: 'Tema1',
+  components: {
+    Muestras, // borrar una vez el componente "Muestras" no se necesite
+  },
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
